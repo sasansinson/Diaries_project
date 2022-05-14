caefcfbb3836e9c6e4afb9fdbed89b358c25e360
sasan sahami <sasan_2002@yahoo.com>
2022-05-14 11:56:39 +1000

    minor changes in styles and mobile friendly

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
 
         dispatch({ type: 'FETCH_ALL', payload: data});
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
@@ -15,9 +15,9 @@ export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
 
-        dispatch({ type: 'CREATE', payload: data}); 
+        dispatch({ type: 'CREATE', payload: data }); 
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         
     }
 }
