cac63aea5cce605c54c954fc7e91c6a980e93906
sasan sahami <sasan_2002@yahoo.com>
2022-05-17 22:02:41 +1000

    minor changes for deploying

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello API');
+});
+
 
 const PORT = process.env.PORT || 5000;
 
